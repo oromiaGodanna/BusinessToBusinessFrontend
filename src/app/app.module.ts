a72bf4719478dacda7dcd1fad31741bb08540aad
oromiaGodanna <ori.gsero@gmail.com>
2020-09-01 16:25:13 +0300

    (feat) Added form for payment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgZorroAntdModule } from 'ng-zorro-antd';
 import { NZ_I18N } from 'ng-zorro-antd/i18n';
 import { en_US } from 'ng-zorro-antd/i18n';
 import { registerLocaleData } from '@angular/common';
@@ -22,7 +23,9 @@ registerLocaleData(en);
   imports: [
     BrowserModule,
     AppRoutingModule,
+    NgZorroAntdModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpClientModule,
     BrowserAnimationsModule
   ],
--- src/app/payment/payment.component.ts
+++ src/app/payment/payment.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormArray, Validators, FormBuilder, ValidationErrors } from '@angular/forms';
+import { Observable } from 'rxjs/internal/Observable';
+import { Observer } from 'rxjs/internal/types';
 
 @Component({
   selector: 'app-payment',
@@ -6,10 +9,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./payment.component.css']
 })
 export class PaymentComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  paymentForm: FormGroup;
+  str = 'Hleo wid';
+  constructor(private fb: FormBuilder) {
+    this.paymentForm = this.fb.group({
+      creditCardId: ['', [Validators.required], [this.creditCardAsyncValidator]]
+    });
   }
 
+  ngOnInit(): void {
+    console.log(this.str.replace(/\s/g, '').length);
+  }
+  creditCardAsyncValidator = (control: FormControl) => new Observable((observer: Observer<ValidationErrors | null>) => {
+    setTimeout(() => {
+      const val = control.value;
+      if (val.replace(/\s/g, '').length !== 16) {
+        // you have to return `{error: true}` to mark it as an error event
+        observer.next({ error: true});
+      } else {
+        observer.next(null);
+      }
+      observer.complete();
+    }, 1000);
+  })
+  onSubmit(value: { userName: string}): void{
+    console.log(value);
+  }
 }
