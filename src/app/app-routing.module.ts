881739dc3324d4d51ae250219e3b638271e7f628
eyerusalem zewdu <eyeruszewdu29@gmail.com>
2020-11-18 22:59:17 +0300

    front end committed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,75 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProductsComponent } from './products/products.component';
+import { AppComponent } from './app.component';
+import { WishlistComponent } from './wishlist/wishlist.component';
+import { CartComponent } from './cart/cart.component';
+import { ProductComponent } from './product/product.component';
+import { AddProductComponent } from './add-product/add-product.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { EditProductComponent } from './edit-product/edit-product.component';
+import { SpecialofferListComponent } from './specialoffer-list/specialoffer-list.component';
+import { PendingSpecialOfferComponent } from './pending-special-offer/pending-special-offer.component';
+import { ActiveSpecialOfferComponent } from './active-special-offer/active-special-offer.component';
+import { IndexComponent } from './index/index.component';
+import { AddCategoryComponent } from './add-category/add-category.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { EditCategoryComponent } from './edit-category/edit-category.component';
+import { AlertsComponent } from './alerts/alerts.component';
+import { ProductByCategoryComponent } from './product-by-category/product-by-category.component';
+import { ProductBySubCategoryComponent } from './product-by-sub-category/product-by-sub-category.component';
+import { SpecialOfferByCategoryComponent } from './special-offer-by-category/special-offer-by-category.component';
+import { SpecialOfferBySubCategoryComponent } from './special-offer-by-sub-category/special-offer-by-sub-category.component';
+import { SpecialOfferProductComponent } from './special-offer-product/special-offer-product.component';
+import { RequestProformaComponent } from './request-proforma/request-proforma.component';
+import { PendingProformaComponent } from './pending-proforma/pending-proforma.component';
+import { OpenedProformaComponent } from './opened-proforma/opened-proforma.component';
+import { ClosedProformaComponent } from './closed-proforma/closed-proforma.component';
+import { ViewProformaComponent } from './view-proforma/view-proforma.component';
+import { ViewResponsesComponent } from './view-responses/view-responses.component';
+import { SearchProductComponent } from './search-product/search-product.component';
+import { MeasurementsComponent } from './measurements/measurements.component';
+import { AddMeasurementComponent } from './add-measurement/add-measurement.component';
+import { FilterProductComponent } from './filter-product/filter-product.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'products',component:ProductsComponent},
+  {path:'wishlist',component:WishlistComponent},
+  {path:'cart',component:CartComponent},
+  { path: 'product/:id', component: ProductComponent},
+  { path: 'addProduct', component: AddProductComponent},
+  { path: 'myProducts', component: ProductListComponent},
+  { path: 'editProduct/:id', component: EditProductComponent},
+  { path: 'pendingSpecialoffer', component: PendingSpecialOfferComponent},
+  { path: 'activeSpecialoffer', component: ActiveSpecialOfferComponent},
+  { path: 'specialOffers', component: SpecialofferListComponent},
+  { path: 'index', component: IndexComponent},
+  { path: 'addCategory', component: AddCategoryComponent},
+  { path: 'categories', component: CategoriesComponent},
+  { path: 'editCategory/:id', component: EditCategoryComponent},
+  { path: 'successMessage/:type/:message', component: AlertsComponent},
+  { path: 'successMessage/:type/:message/:route', component: AlertsComponent},
+  { path: 'productByCategory/:category', component: ProductByCategoryComponent},
+  { path: 'productBySubCategory/:subCategory', component: ProductBySubCategoryComponent},
+  { path: 'specialOfferByCategory/:category', component: SpecialOfferByCategoryComponent},
+  { path: 'specialOfferBySubCategory/:subCategory', component: SpecialOfferBySubCategoryComponent},
+  { path: 'specialOfferProduct/:id', component: SpecialOfferProductComponent},
+  { path: 'requestProforma', component: RequestProformaComponent},
+  { path: 'pendingProformas', component: PendingProformaComponent},
+  { path: 'pendingProforma/:message', component: PendingProformaComponent},
+  { path: 'activeProformas', component: OpenedProformaComponent},
+  { path: 'activeProformas/:message', component: OpenedProformaComponent},
+  { path: 'closedProformas', component: ClosedProformaComponent},
+  { path: 'closedProformas/:message', component: ClosedProformaComponent},
+  { path: 'viewProforma/:proformaId', component: ViewProformaComponent},
+  { path: 'viewResponses/:itemId', component: ViewResponsesComponent},
+  { path: 'searchForProduct', component: SearchProductComponent},
+  { path: 'searchForProduct/:searchWord', component: SearchProductComponent},
+  { path: 'measurements', component: MeasurementsComponent},
+  { path: 'addMeasurement', component: AddMeasurementComponent},
+  { path: 'filterProducts/:productCategory/:productSubCategory/:maxPrice', component: FilterProductComponent},
+  {path:'*',component:IndexComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,161 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NZ_I18N } from 'ng-zorro-antd/i18n';
-import { en_US } from 'ng-zorro-antd/i18n';
-import { registerLocaleData } from '@angular/common';
-import en from '@angular/common/locales/en';
+import { ProductsComponent } from './products/products.component';
+import { WishlistComponent } from './wishlist/wishlist.component';
+import { ProductsService } from './services/products.service';
+import { WishlistService } from './services/wishlist.service';
+import { CategoryService } from './services/category.service';
+import { SpecialofferService } from './services/specialoffer.service';
+import { ProformaService } from './services/proforma.service';
+import { CartService } from './services/cart.service';
+import { ResponseService } from './services/response.service';
+import { MeasurementsService } from './services/measurements.service';
+import { ProductComponent } from './product/product.component';
+import { CartComponent } from './cart/cart.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { EditProductComponent } from './edit-product/edit-product.component';
+import { SpecialofferListComponent } from './specialoffer-list/specialoffer-list.component';
 
-registerLocaleData(en);
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { NzBreadCrumbModule } from 'ng-zorro-antd/breadcrumb';
+import { NzCarouselModule } from 'ng-zorro-antd/Carousel';
+import { NzCardModule } from 'ng-zorro-antd/card';
+import { NzGridModule } from 'ng-zorro-antd/grid';
+import { NzSpaceModule } from 'ng-zorro-antd/space';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzTabsModule } from 'ng-zorro-antd/tabs';
+import { NzTypographyModule } from 'ng-zorro-antd/typography';
+import { NzNotificationModule } from 'ng-zorro-antd/notification';
+import { AddProductComponent } from './add-product/add-product.component';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzSelectModule } from 'ng-zorro-antd/select';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzUploadModule } from 'ng-zorro-antd/upload';
+import { NzTableModule } from 'ng-zorro-antd/table';
+import { NzDividerModule } from 'ng-zorro-antd/divider';
+import { NzModalModule } from 'ng-zorro-antd/modal';
+import { NzMessageModule } from 'ng-zorro-antd/message';
+import { NzDatePickerModule } from 'ng-zorro-antd/date-picker';
+import { ActiveSpecialOfferComponent } from './active-special-offer/active-special-offer.component';
+import { PendingSpecialOfferComponent } from './pending-special-offer/pending-special-offer.component';
+import { NzInputNumberModule } from 'ng-zorro-antd/input-number';
+import { IndexComponent } from './index/index.component';
+import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
+import { AddCategoryComponent } from './add-category/add-category.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { EditCategoryComponent } from './edit-category/edit-category.component';
+import { AlertsComponent } from './alerts/alerts.component';
+import { NzSpinModule } from 'ng-zorro-antd/spin';
+import { NzResultModule } from 'ng-zorro-antd/result';
+import { ProductByCategoryComponent } from './product-by-category/product-by-category.component';
+import { ProductBySubCategoryComponent } from './product-by-sub-category/product-by-sub-category.component';
+import { SpecialOfferByCategoryComponent } from './special-offer-by-category/special-offer-by-category.component';
+import { SpecialOfferBySubCategoryComponent } from './special-offer-by-sub-category/special-offer-by-sub-category.component';
+import { SpecialOfferProductComponent } from './special-offer-product/special-offer-product.component';
+import { RequestProformaComponent } from './request-proforma/request-proforma.component';
+import { PendingProformaComponent } from './pending-proforma/pending-proforma.component';
+import { ClosedProformaComponent } from './closed-proforma/closed-proforma.component';
+import { ViewProformaComponent } from './view-proforma/view-proforma.component';
+import { OpenedProformaComponent } from './opened-proforma/opened-proforma.component';
+import { ViewResponsesComponent } from './view-responses/view-responses.component';
+import { NzAvatarModule } from 'ng-zorro-antd/avatar';
+import { NzCommentModule } from 'ng-zorro-antd/comment';
+import { SearchProductComponent } from './search-product/search-product.component';
+import { FilterProductComponent } from './filter-product/filter-product.component';
+import { MeasurementsComponent } from './measurements/measurements.component';
+import { AddMeasurementComponent } from './add-measurement/add-measurement.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    WishlistComponent,
+    ProductComponent,
+    CartComponent,
+    AddProductComponent,
+    ProductListComponent,
+    EditProductComponent,
+    SpecialofferListComponent,
+    ActiveSpecialOfferComponent,
+    PendingSpecialOfferComponent,
+    IndexComponent,
+    AddCategoryComponent,
+    CategoriesComponent,
+    EditCategoryComponent,
+    AlertsComponent,
+    ProductByCategoryComponent,
+    ProductBySubCategoryComponent,
+    SpecialOfferByCategoryComponent,
+    SpecialOfferBySubCategoryComponent,
+    SpecialOfferProductComponent,
+    RequestProformaComponent,
+    PendingProformaComponent,
+    ClosedProformaComponent,
+    ViewProformaComponent,
+    OpenedProformaComponent,
+    ViewResponsesComponent,
+    SearchProductComponent,
+    FilterProductComponent,
+    MeasurementsComponent,
+    AddMeasurementComponent
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    FormsModule,
+    ReactiveFormsModule,
+    NzButtonModule,
+    NzLayoutModule,
+    NzMenuModule,
+    NzBreadCrumbModule,
+    NzCarouselModule,
+    NzCardModule,
+    NzGridModule,
+    NzSpaceModule,
+    NzIconModule,
+    NzTabsModule,
+    NzTypographyModule,
+    NzNotificationModule,
+    NzFormModule,
+    NzSelectModule,
+    BrowserAnimationsModule,
+    NzInputModule,
+    NzUploadModule,
+    NzTableModule,
+    NzDividerModule,
+    NzModalModule,
+    NzMessageModule,
+    NzDatePickerModule,
+    NzInputNumberModule,
+    NzDropDownModule,
+    NzSpinModule,
+    NzResultModule,
+    NzAvatarModule,
+    NzCommentModule,
+    
   ],
-  providers: [{ provide: NZ_I18N, useValue: en_US }],
+  providers: [
+    ProductsService,
+    WishlistService,
+    CategoryService,
+    CartService,
+    CategoryService,
+    SpecialofferService,
+    ProformaService,
+    ResponseService,
+    MeasurementsService
+    
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
