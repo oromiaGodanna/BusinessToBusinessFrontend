5d98b180ce28e44677670eac32c1e44139aec353
Mercy <tamenemihret@gmail.com>
2020-12-05 21:31:48 +0300

    changes to the help page

--- src/app/help/help.component.ts
+++ src/app/help/help.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AppHttpService } from '../services/app-http.service';
 
 @Component({
   selector: 'app-help',
@@ -8,44 +9,48 @@ import { Component, OnInit } from '@angular/core';
 export class HelpComponent implements OnInit {
 
   searchValue: string;
-  selectedRole: string = 'Buyer';
-  popularTopics = [
-    {
-      title: 'Getting Started',
-      icon: 'rocket',
-      content: 'Become a member for free'
-    },
-    {
-      title: 'Account',
-      icon: 'user',
-      content: 'How to Manage Your Account'
-    },
-    {
-      title: 'Search For Products',
-      icon: 'search',
-      content: 'Find what your are looking for'
-    },
-    {
-      title: 'Messaging',
-      icon: 'message',
-      content: 'Stay connected with you customers'
-    },
-    {
-      title: 'Proforma',
-      icon: 'file',
-      content: 'Reach out to multiple vendors to get the best deal'
-    },
-    {
-      title: 'Ordering',
-      icon: 'shopping-cart',
-      content: 'Make a purchase inside the comfort of your home'
-    },
-    {
-      title: 'Payment',
-      icon: 'wallet',
-      content: 'Complete your transaction through our online payment system'
-    }
-  ];
+  selectedRole: string = 'All';
+  topics = [];
+  popularTopics = [];
+  selectedTopic;
+  selectedTask;
+  // popularTopics = [
+  //   {
+  //     title: 'Getting Started',
+  //     icon: 'rocket',
+  //     content: 'Become a member for free'
+  //   },
+  //   {
+  //     title: 'Account',
+  //     icon: 'user',
+  //     content: 'How to Manage Your Account'
+  //   },
+  //   {
+  //     title: 'Search For Products',
+  //     icon: 'search',
+  //     content: 'Find what your are looking for'
+  //   },
+  //   {
+  //     title: 'Messaging',
+  //     icon: 'message',
+  //     content: 'Stay connected with you customers'
+  //   },
+  //   {
+  //     title: 'Proforma',
+  //     icon: 'file',
+  //     content: 'Reach out to multiple vendors to get the best deal'
+  //   },
+  //   {
+  //     title: 'Ordering',
+  //     icon: 'shopping-cart',
+  //     content: 'Make a purchase inside the comfort of your home'
+  //   },
+  //   {
+  //     title: 'Payment',
+  //     icon: 'wallet',
+  //     content: 'Complete your transaction through our online payment system'
+  //   }
+  // ];
 
   FAQs = [
     {
@@ -75,13 +80,35 @@ export class HelpComponent implements OnInit {
     this.isCollapsed = !this.isCollapsed;
   }
 
-  constructor() { }
+  constructor(private http: AppHttpService) { }
 
   ngOnInit(): void {
+    this.http.get('http://localhost:3000/tasks/').subscribe(
+      (response:any) => {
+        if(response.status == 200){
+          this.topics = response.topics;
+          this.popularTopics = this.topics.filter(topic => topic.popular == true);
+          this.selectedTopic =  this.topics[0];
+          this.selectedTask = this.selectedTopic.tasks[0];
+          console.log(this.selectedTask);
+          console.log(this.selectedTopic);
+        
+        }
+      }, (error) => {
+        console.log(error);
+      })
   }
 
   onSearch() {
     console.log(this.searchValue);
+
+  }
+
+  onSelectTask(topic_i, task_i){
+    this.selectedTopic = this.topics[topic_i];
+    this.selectedTask = this.selectedTopic.tasks[task_i];
+    console.log('selected topic', this.selectedTopic);
+    console.log('selectedtask', this.selectedTask);
 
   }
 
